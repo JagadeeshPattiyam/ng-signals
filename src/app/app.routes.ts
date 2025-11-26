3bc57e03f3bcc63c5e5e893c9e76a36ab9dcf309
Jagadeesh PA <jagadeesh.p@technogradient.com>
2025-11-26 12:37:11 +0530

    App file modified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { SignalsComponent } from './signals/signals.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {path:'',component:SignalsComponent}
+];
